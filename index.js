a9f6f939bb01175306b04d308ef559a245a47935
Richard Albert Leddy <richardaleddy@gmail.com>
2021-07-08 18:51:38 -0700

    Move class files to lib dir, create caching class
    A class for creating a dir sync has been made with the idea that RAM disk will be the preferred storage medium for meta data.

---
+++ index.js
@@ -0,0 +1,11 @@
+let UserCategory = require('./lib/users')
+let PersistenceCategory = require('./lib/persistence')
+let CachingProcess = require('./lib/caching_process')
+
+
+
+module.exports = {
+    "UserCategory" : UserCategory,
+    "PersistenceCategory" : PersistenceCategory,
+    "CachingProcess" : CachingProcess
+}
