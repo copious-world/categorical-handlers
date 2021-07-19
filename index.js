ee9aec3c2941cb0b0b989d21ded20f25a46f110a
Richard Albert Leddy <richardaleddy@gmail.com>
2021-07-19 12:08:15 -0700

    Add an extension to the message relay class
    Add create_on_path and update_on_path

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let UserCategory = require('./lib/users')
 let PersistenceCategory = require('./lib/persistence')
 let CachingProcess = require('./lib/caching_process')
+let ExtendedMessageRelayer = require('./lib/extended_relay')
 
 
 
 module.exports = {
     "UserCategory" : UserCategory,
     "PersistenceCategory" : PersistenceCategory,
-    "CachingProcess" : CachingProcess
+    "CachingProcess" : CachingProcess,
+    "MessageRelayer" : ExtendedMessageRelayer
 }
