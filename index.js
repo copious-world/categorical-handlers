561bf599f4001485a784c399c516279e80c3cceb
Richard Albert Leddy <richardaleddy@gmail.com>
2021-09-05 16:43:11 -0700

    caching process has been moved to a higher level Registry

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 let UserCategory = require('./lib/users')
 let PersistenceCategory = require('./lib/persistence')
-let CachingProcess = require('./lib/caching_process')
 let {ExtendedMessageRelayer,ExtendedMultiPathRelayClient} = require('./lib/extended_relay')
-
-
+//
 
 module.exports = {
     "UserCategory" : UserCategory,
     "PersistenceCategory" : PersistenceCategory,
-    "CachingProcess" : CachingProcess,
     "MessageRelayer" : ExtendedMessageRelayer,
     "MultiPathRelayClient" : ExtendedMultiPathRelayClient
 }
