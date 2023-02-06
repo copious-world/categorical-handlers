83a665be06b03112526345438de47f58f57d82dd
Richard Albert Leddy <richardaleddy@gmail.com>
2023-02-05 23:05:25 -0800

    Providing endpoints that can use IPC communication.
    Now with three versions of persistence, done by selecting classes to extend. This could be better with less code copying. A task for later

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let UserCategory = require('./lib/users')
 let PersistenceCategory = require('./lib/persistence')
 let PersistenceCachingCategory = require('./lib/persistence_caching')
+let PersistenceCachingIPCCategory = require('./lib/persistence_caching_IPC')
 let {ExtendedMessageRelayer,ExtendedMultiPathRelayClient} = require('./lib/extended_relay')
 //
 
@@ -8,6 +9,7 @@ module.exports = {
     "UserCategory" : UserCategory,
     "PersistenceCategory" : PersistenceCategory,
     "PersistenceCachingCategory" : PersistenceCachingCategory,
+    "PersistenceCachingIPCCategory" : PersistenceCachingIPCCategory,
     "MessageRelayer" : ExtendedMessageRelayer,
     "MultiPathRelayClient" : ExtendedMultiPathRelayClient
 }
